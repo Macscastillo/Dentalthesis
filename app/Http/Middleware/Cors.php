55bebe6421130b7bfb02da2b6fa17942406caf8c
MacsCastillo <58358711+Macscastillo@users.noreply.github.com>
2021-01-23 04:08:17 +0800

    new

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -7,12 +7,12 @@ use Illuminate\Http\Request;
 
 class Cors
 {
-    public function handle(Request $request, Closure $next)
+    public function handle($request, Closure $next)
     {
         return $next($request)
-            ->header('Access-Control-Allow-Origin', "*")
-            ->header('Access-Control-Allow-Methods', "PUT,POST,DELETE,GET,OPTION")
-            ->header('Access-Control-Allow-Headers', "X-Requested-With,Content-Type,X-Token-Auth,Authorization,Accept");
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'PUT,POST,DELETE,GET,OPTION')
+            ->header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,X-Token-Auth,Authorization,Accept');
     }
 }
 
