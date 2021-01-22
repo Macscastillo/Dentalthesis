127a7a9d708776831ef6accf9a5898759bb4ca13
MacsCastillo <58358711+Macscastillo@users.noreply.github.com>
2021-01-23 03:18:42 +0800

    new

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,9 +17,8 @@ class Cors
     public function handle(Request $request, Closure $next)
     {
         return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', 'PUT,POST,DELETE,GET,OPTION')
-            ->header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,X-Token-Auth,Authorization');
+            ->header('Access-Control-Allow-Origin', "*")
+            ->header('Access-Control-Allow-Methods', "PUT,POST,DELETE,GET,OPTION")
+            ->header('Access-Control-Allow-Headers', "X-Requested-With,Content-Type,X-Token-Auth,Authorization,Accept");
     }
 }
-?>
