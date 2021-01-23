2e53f78f449a3891565add5ff3ff652dcc509d57
MacsCastillo <58358711+Macscastillo@users.noreply.github.com>
2021-01-23 11:48:23 +0800

    try

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -9,10 +9,26 @@ class Cors
 {
     public function handle($request, Closure $next)
     {
-        return $next($request)
-        ->header('Access-Control-Allow-Origin', '*')
-        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-        ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization');
+    //     return $next($request)
+    //     ->header('Access-Control-Allow-Origin', '*')
+    //     ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+    //     ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization');
+    // }
+
+        header("Access-Control-Allow-Origin: *");
+        $headers = [
+            'Access-Control-Methods' => 'POST, GET , OPTION, PUT, DELETE',
+            'Access-Control-Allow-Headers' => 'Content-Type, X-Auth-Token, Origin, Authorization',
+        ];
+
+        if($reqest->getMethod() == "OPTIONS"){
+            return response()->json('OK', 200, $headers);
+        }
+        $response = $next($request);
+        foreach($headeres as $key => $value){
+            $response->header($key, $value);
+        }
+        return $response;
     }
 }
 
